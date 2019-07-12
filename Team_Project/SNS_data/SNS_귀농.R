f6e5e99d1e9d789f6e3e49d40d6d4a167c1bd8eb
Kimyeongjin <dudwlsrla21@naver.com>
2019-07-12 09:15:48 +0900

    Project

--- Team_Project/SNS_data/SNS_귀농.R
+++ Team_Project/SNS_data/SNS_귀농.R
@@ -1,8 +1,8 @@
 getwd()
-
-install.packages('xlsx')
-library(xlsx)
-
+#https://jaihak.blogspot.com/2017/03/blog-post_94.html
 news <- read.csv('NewsResult_20120101-20190710.csv')
 str(news)
 dim(news)
+
+news$
+
